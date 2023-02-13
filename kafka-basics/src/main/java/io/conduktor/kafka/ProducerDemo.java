ab90ca0277b14d24d2ccf48dfefc3ce749ebb111
Li <wendyli8620lw@gmail.com>
2023-02-13 19:12:52 +0100

    init

---
+++ kafka-basics/src/main/java/io/conduktor/kafka/ProducerDemo.java
@@ -0,0 +1,35 @@
+package io.conduktor.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("hello world");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+
+        // flush and close the producer
+    }
+}
